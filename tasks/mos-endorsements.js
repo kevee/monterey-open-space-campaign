6181a5ab5803097a5c25fa7074e232cc2d74f1e1
Kevin Miller <kevin@csumb.edu>
2016-07-09 13:26:54 -0700

    Added travis.yml. Got endorsement data done.

--- tasks/mos-endorsements.js
+++ tasks/mos-endorsements.js
@@ -2,19 +2,18 @@
 
 module.exports = function(grunt) {
 
-  var Prismic = require('prismic.io');
+  var Prismic = require('prismic.io').Prismic;
 
   grunt.registerMultiTask('mos-endorsements', 'Add endorsements JSON file', function() {
-    var that = this;
-    var endorsements = {};
-    Prismic.api(this.data.endpoint, function (err, api) {
-      var options = {};
-      console.log(api);
-      api.query("", options, function(error, response) { // An empty query will return all the documents
-        if (error) {
-          console.log("Something went wrong: ", err);
-        }
-        console.log("Documents: ", response.documents);
+    var that = this,
+        endorsements = {},
+        done = this.async();
+    Prismic.Api(that.data.endpoint, function (err, Api) {
+      Api.form('everything')
+        .ref(Api.master())
+        .query(Prismic.Predicates.at("document.type", "endorsement")).submit(function (err, response) {
+          grunt.file.write(that.data.data, JSON.stringify(response.results));
+          done();
       });
     });
   });
