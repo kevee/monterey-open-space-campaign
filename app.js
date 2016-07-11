1b7ea62d7245b3a721abf6786e94bc8b5de14849
Kevin Miller <kevin@csumb.edu>
2016-07-11 09:30:13 -0700

    Setup build hook.

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+    https = require('https');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/*',function(request, response, next) {
+  response.set({
+  'Content-Type': 'text/json'
+});
+  next();
+});
+
+app.get('/webhook', function(req, res) {
+
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
